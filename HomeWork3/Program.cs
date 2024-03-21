7cda380f89e372f3cb7dc6425f3beba1e92f3de9
Matveykylak <matveykylak@gmail.com>
2024-03-21 23:28:38 +0300

    Second task

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -14,7 +14,7 @@ sign.
 И если таковая имеется, то отмените выполнение арифметической операции и
 уведомите об ошибке пользователя.*/
 
-Console.WriteLine("Enter first operand");
+/*Console.WriteLine("Enter first operand");
 double operand1 = double.Parse(Console.ReadLine());
 
 Console.WriteLine("Enter a math operation\n\'+\' \'-\' \'*\' \'/\' \'%\'");
@@ -52,4 +52,35 @@ switch (sign) {
             Console.WriteLine("Something wrong");
             break;
 
-    }
+    }*/
+
+//Second task numerical interval
+/*Задание 2
+Напишите программу определения, попадает ли указанное пользователем число от 0 до
+100 в числовой промежуток [0 - 14] [15 - 35] [36 - 50][50 - 100].
+Если да, то укажите, в какой именно промежуток.
+Если пользователь указывает число, не входящее ни в один из имеющихся числовых
+промежутков, то выводится соответствующее сообщение.Задание 2
+Напишите программу определения, попадает ли указанное пользователем число от 0 до
+100 в числовой промежуток [0 - 14] [15 - 35] [36 - 50][50 - 100].
+Если да, то укажите, в какой именно промежуток.
+Если пользователь указывает число, не входящее ни в один из имеющихся числовых
+промежутков, то выводится соответствующее сообщение.*/
+const int limitNumber1 = 0;
+const int limitNumber2 = 100;
+
+Console.WriteLine($"Enter integer number to check from {limitNumber1} to {limitNumber2}");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number < limitNumber1) Console.WriteLine($"Going beyond the limit\n{number}<{limitNumber1}");
+
+if (number > limitNumber2) Console.WriteLine($"Going beyond the limit\n{number}>{limitNumber2}");
+
+if (number >= limitNumber1 && number <= 14) Console.WriteLine("The number is included in the interval from 0 to 14");
+
+if (number >= 15 && number <= 35) Console.WriteLine("The number is included in the interval from 15 to 35");
+
+if (number >= 36 && number <= 50) Console.WriteLine("The number is included in the interval from 36 to 50");
+
+if (number >= 50 && number <= 100) Console.WriteLine("The number is included in the interval from 50 to 100");
